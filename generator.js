c7c5af805ef4d0269b71f720c2030d675ec656b2
Andrew Whiskin <andrew.whiskin@gmail.com>
2019-08-28 15:52:50 +1000

    Updated .js file to fix ESLint errors

--- generator.js
+++ generator.js
@@ -5,7 +5,7 @@ var passwordArray = [];
 var url = "https://raw.githubusercontent.com/awhiskin/Password-Generator/master/animals.json";
 
 // The response from the GET request
-var response = Get(url);
+var response = get(url);
 
 // JSON object parsed from the response
 var myObj = JSON.parse(response);
@@ -21,13 +21,17 @@ var defaultText = "<p>No passwords :(</p>";
 // ------------------ CODE ----------------------------
 
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+    'use strict';
+    
 	var listElement = document.getElementById("generated-passwords-list");
-  listElement.innerHTML = defaultText;
+    listElement.innerHTML = defaultText;
 });
 
 document.addEventListener('click', function (event) {
-	if (!event.target.matches('ul li')) return;
+    'use strict';
+    
+	if (!event.target.matches('ul li')) { return; }
 
 	// Copy selected password string to clipboard
 	copyStringToClipboard(event.target.innerHTML);
@@ -37,91 +41,106 @@ document.addEventListener('click', function (event) {
 // ------------------ FUNCTIONS -----------------------
 
 // Sends a GET request for a specified URL
-function Get(yourUrl) {
-  var Httpreq = new XMLHttpRequest(); // a new request
-  Httpreq.open("GET", yourUrl, false);
-  Httpreq.send(null);
+function get(yourUrl) {
+    'use strict';
+    
+    var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET", yourUrl, false);
+    Httpreq.send(null);
 
-  return Httpreq.responseText;
+    return Httpreq.responseText;
 }
 
 // Returns a random element from the passed array or returns null if no array is passed
 function getRandomElementFromArray(array) {
-  if (array === null || array === undefined) {
-    return null;
-  } else {
-    var randomNumber = getRandomInt(0, array.length);
-    return array[randomNumber];
-  }
+    'use strict';
+    
+    if (array === null || array === undefined) {
+        return null;
+    } else {
+        var randomNumber = getRandomInt(0, array.length);
+        return array[randomNumber];
+    }
 }
 
 // Gets a random integer between min and max
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+    'use strict';
+    
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
 // Clears passwords from the array and from the page
 function clearPasswords() {
-	passwordArray = [];
+    'use strict';
 
-  var listElement = document.getElementById("generated-passwords-list");
-  listElement.innerHTML = defaultText;
+    passwordArray = [];
+
+    var listElement = document.getElementById("generated-passwords-list");
+    listElement.innerHTML = defaultText;
 }
 
 // Copies the passed string to the clipboard
 function copyStringToClipboard(str) {
-   // Create new element
-   var el = document.createElement('textarea');
-   // Set value (string to be copied)
-   el.value = str;
-   // Set non-editable to avoid focus and move outside of view
-   el.setAttribute('readonly', '');
-   el.style = {position: 'absolute', left: '-9999px'};
-   document.body.appendChild(el);
-   // Select text inside element
-   el.select();
-   // Copy text to clipboard
-   document.execCommand('copy');
-   // Remove temporary element
-   document.body.removeChild(el);
+    'use strict';
+
+    // Create new element
+    var element = document.createElement('textarea');
+    // Set value (string to be copied)
+    element.value = str;
+    // Set non-editable to avoid focus and move outside of view
+    element.setAttribute('readonly', '');
+    element.style = {position: 'absolute', left: '-9999px'};
+    document.body.appendChild(element);
+    // Select text inside element
+    element.select();
+    // Copy text to clipboard
+    document.execCommand('copy');
+    // Remove temporary element
+    document.body.removeChild(element);
 }
 
 // Generates a password using a random combination of positive adjectives and animals with numbers at the end
 function generatePassword() {
-  var positive = getRandomElementFromArray(positives);
-  var animal = getRandomElementFromArray(animals);
-  var password = positive + animal + getRandomInt(0, 9);
-  
-  if (password.length <= 8) {
-	var remaining = 8 - password.length;
-	for (i = 0; i < remaining; ++i) {
-		password = password + getRandomInt();
-	}
-  }
+    'use strict';
 
-  passwordArray.unshift(password);
-  
-  if (passwordArray.length > 10) {
-	passwordArray.pop();
-  }
-  
-  // Copy the current password to the clipboard for ease of use
-  copyStringToClipboard(password);
+    var positive, animal, password, remaining, i, listElement, listItem;
 
-  var listElement = document.getElementById("generated-passwords-list");
-  listElement.innerHTML = "";
+    positive = getRandomElementFromArray(positives);
+    animal = getRandomElementFromArray(animals);
 
-  for (i = 0; i < passwordArray.length; ++i) {
-    // create an item for each one
-    var listItem = document.createElement('li');
+    password = positive + animal + getRandomInt(0, 9);
 
-    // Add the item text
-    listItem.innerHTML = passwordArray[i];
+    if (password.length <= 8) {
+        remaining = 8 - password.length;
+        for (i = 0; i < remaining; i += 1) {
+            password = password + getRandomInt();
+        }
+    }
+
+    passwordArray.unshift(password);
+
+    if (passwordArray.length > 10) {
+        passwordArray.pop();
+    }
+
+    // Copy the current password to the clipboard for ease of use
+    copyStringToClipboard(password);
+
+    listElement = document.getElementById("generated-passwords-list");
+    listElement.innerHTML = "";
+
+    for (i = 0; i < passwordArray.length; i += 1) {
+        // create an item for each one
+        listItem = document.createElement('li');
+
+        // Add the item text
+        listItem.innerHTML = passwordArray[i];
 
 
-    // Add listItem to the listElement
-    listElement.appendChild(listItem);
-  }
+        // Add listItem to the listElement
+        listElement.appendChild(listItem);
+    }
 }
